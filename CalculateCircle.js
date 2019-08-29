1e560fdcb3854a67148dacbec6f50026404ef3ee
milomacphail <milomacphail@gmail.com>
2019-08-29 08:22:02 -0400

    Day One

---
+++ CalculateCircle.js
@@ -0,0 +1,16 @@
+
+function main() {
+
+    const PI = Math.PI;
+
+    let r = readLine();
+    
+
+    let area = (r*r) * PI;
+    console.log(area);
+    
+
+    let perimeter = 2 * (PI * r);
+    console.log(perimeter);
+
+}
